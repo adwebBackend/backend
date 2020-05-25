7c764da0e966bf40eaeca81f659747f8f46d4c48
zdz <17302010033@fudan.edu.cn>
2020-05-25 12:18:29 +0800

    userController

--- src/main/java/fudan/se/project/controller/UserController.java
+++ src/main/java/fudan/se/project/controller/UserController.java
@@ -5,10 +5,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import fudan.se.project.domain.User;
 import fudan.se.project.service.CourseService;
 //import fudan.se.project.service.FileService;
+import fudan.se.project.service.FileService;
 import fudan.se.project.service.UserService;
 import fudan.se.project.tool.Tool;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Repository;
@@ -20,7 +23,9 @@ import javax.imageio.ImageIO;
 import javax.persistence.Table;
 import javax.servlet.http.HttpServletRequest;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -35,8 +40,8 @@ public class UserController {
     private UserService userService;
     @Autowired
     private CourseService courseService;
-//    @Autowired
-//    private FileService fileService;
+    @Autowired
+    private FileService fileService;
 //测试
     @Autowired
     public UserController(UserService userService){this.userService = userService;}
@@ -119,37 +124,45 @@ public class UserController {
         return new ResponseEntity<>(result.toJSONString(),HttpStatus.OK);
     }
 
-//    @PostMapping("/modify_avatar")
-//    @ResponseBody
-//    public ResponseEntity<?> modifyAvatar(@Validated @RequestParam(value = "avatar") MultipartFile avatar) throws IOException {
-//        JSONObject message = new JSONObject();
-//        //检查是否是图片
-//        BufferedImage bi = ImageIO.read(avatar.getInputStream());
-//        if (bi == null){
-//            message.put("message","An image is required");
-//            return new ResponseEntity<>(message.toJSONString(),HttpStatus.BAD_REQUEST);
-//        }
-//        //检查图片大小
-//        if (avatar.getSize() > 100){
-//            message.put("message","Image is too large");
-//            return new ResponseEntity<>(message.toJSONString(),HttpStatus.BAD_REQUEST);
-//        }
+    @PostMapping("/modify_avatar")
+    @ResponseBody
+    public ResponseEntity<?> modifyAvatar(@Validated @RequestParam(value = "avatar") MultipartFile avatar) throws IOException {
+        JSONObject message = new JSONObject();
+        //检查是否是图片
+        BufferedImage bi = ImageIO.read(avatar.getInputStream());
+        if (bi == null){
+            message.put("message","An image is required");
+            return new ResponseEntity<>(message.toJSONString(),HttpStatus.BAD_REQUEST);
+        }
+        //检查图片大小
+        float size = Float.parseFloat(String.valueOf(avatar.getSize())) / 1024;
+        BigDecimal b = new BigDecimal(size);
+        // 2表示2位 ROUND_HALF_UP表明四舍五入，
+        size = b.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
+        if (size > 200){
+            message.put("message","Image is too large");
+            return new ResponseEntity<>(message.toJSONString(),HttpStatus.BAD_REQUEST);
+        }
 //        int userId = Integer.parseInt((((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername()));
-//        String path = fileService.saveFile(avatar);
-//        String result = userService.modifyAvatar(userId,path);
-//        if (result.equals("success")){
-//            return new ResponseEntity<>(result,HttpStatus.OK);
-//        }
-//        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-//    }
+        int userId = 1;
+        String path = fileService.saveFile(avatar);
+        String result = userService.modifyAvatar(userId,path);
+        if (result.equals("success")){
+            return new ResponseEntity<>(result,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
 
     @GetMapping("/view_avatar")
     @ResponseBody
-    public ResponseEntity<?> viewAvatar(){
-        int userId = Integer.parseInt((((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername()));
+    public ResponseEntity<?> viewAvatar() throws Exception {
+//        int userId = Integer.parseInt((((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername()));
+        int userId = 1;
         String result = userService.viewAvatar(userId);
         if (result.equals("failure"))
             return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
+
         return new ResponseEntity<>(result,HttpStatus.OK);
     }
+
 }
