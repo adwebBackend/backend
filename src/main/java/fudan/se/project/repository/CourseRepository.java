d03f6edbd4dfad8dde3d25eb427061462e5942ed
zdz <17302010033@fudan.edu.cn>
2020-05-26 17:11:43 +0800

    fix repository

--- src/main/java/fudan/se/project/repository/CourseRepository.java
+++ src/main/java/fudan/se/project/repository/CourseRepository.java
@@ -3,6 +3,7 @@ package fudan.se.project.repository;
 import fudan.se.project.domain.Course;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,7 @@ public interface CourseRepository extends CrudRepository<Course,Integer> {
     Course findByCourseId(int courseId);
 
     @Query(value = "select * from course where courseId not in(:limited)", nativeQuery = true)
-    List<Course> findCourseByLimited(List<Integer> limited);
+    List<Course> findCourseByLimited(@Param("limited")List<Integer> limited);
 
     void deleteByCourseId(int courseId);
 }
--- src/main/java/fudan/se/project/repository/ParticipateRepository.java
+++ src/main/java/fudan/se/project/repository/ParticipateRepository.java
@@ -4,6 +4,7 @@ import fudan.se.project.domain.Course;
 import fudan.se.project.domain.Participate;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,10 +12,10 @@ import java.util.List;
 @Repository
 public interface ParticipateRepository extends CrudRepository<Participate,Long> {
     @Query(value = "select * from participate where userId =:userId and projectId in(:limited)", nativeQuery = true)
-    List<Participate> findParticipateByUserIdAndLimited(int userId,List<Integer> limited);
+    List<Participate> findParticipateByUserIdAndLimited(@Param("userId") int userId, @Param("limited") List<Integer> limited);
 
     @Query(value = "select * from participate where userId =:userId and projectId not in(:limited)", nativeQuery = true)
-    List<Participate> findParticipateByUserIdAndNotLimited(int userId,List<Integer> limited);
+    List<Participate> findParticipateByUserIdAndNotLimited(@Param("userId") int userId,@Param("limited") List<Integer> limited);
     List<Participate> findAllByProjectId(int projectId);
     Participate findByProjectIdAndUserId(int projectId,int userId);
 }
